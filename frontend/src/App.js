1b39f126d2a57bcc2fc3f6a384923c012a2d131e
PalaciosZ <criszelaya24@hotmail.com>
2019-04-02 16:42:52 -0600

    making work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 class App extends Component  {
   render(){
-    return ({
+    return (
       <div>
-        <p>Working on progress</p>
+        <p>working on progress</p>
       </div>
-    })
+    )
   }
 }
 
