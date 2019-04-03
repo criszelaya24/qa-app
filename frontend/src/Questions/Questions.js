3b345c4118f91e661461a5decd981264180f7b5e
PalaciosZ <criszelaya24@hotmail.com>
2019-04-03 12:05:33 -0600

    Creating questions component and connecting with backend

---
+++ frontend/src/Questions/Questions.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+class Questions extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      questions: null,
+    };
+  }
+  asyn componentDidMount() {
+    const questions = (await axios.get('http://localhost:8080/')).data;
+    this.setState({
+      questions: questions,
+    });
+  }
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+          {this.state.questions === null && <p>Loading questions..</p>}
+          {
+            this.state.questions && this.state.questions.map( q => (
+              <div key={q.id} className="col-sm-12 col-md-4 col-lg-3">
+                <Link to={'/question/${q.id}'}>
+                  <div className="card text-white bg-success mb-3">
+                    <div className="card-header">
+                      Answer: {q.answer}
+                      <div className="card-body">
+                        <h4 className="card-title">{q.title}</h4>
+                        <p className="card-text">{q.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+}
+export default Questions;
