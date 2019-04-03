2dfc079f2ab75db5f42caac5759839b81c54a011
PalaciosZ <criszelaya24@hotmail.com>
2019-04-03 12:28:46 -0600

    Showing individual question

--- frontend/src/Questions/Questions.js
+++ frontend/src/Questions/Questions.js
@@ -23,7 +23,7 @@ class Questions extends Component {
           {
             this.state.questions && this.state.questions.map( q => (
               <div key={q.id} className="col-sm-12 col-md-4 col-lg-3">
-                <Link to={'/question/${q.id}'}>
+                <Link to={`/question/${q.id}`}>
                   <div className="card text-white bg-success mb-3">
                     <div className="card-header">
                       Answer: {q.answer}
